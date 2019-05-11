f054a1756026dba5a55ae2bb4e8831eb63b30fda
sark245 <sark24599@gmail.com>
2019-05-11 14:17:50 +0530

    My first Commit

---
+++ main.c
@@ -0,0 +1,12 @@
+int main(int argc, char **argv){
+    //load config files, if any.
+    
+    //Run command Loop
+    
+    lsh_loop();
+
+    //Perform any shutdown or cleanup.
+    
+    return EXIT_STATUS;
+}
+
