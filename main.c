bcee0cd6f1084e737d51b6211bfca2849d53a5a3
sark245 <sark24599@gmail.com>
2019-05-11 14:43:35 +0530

    Defined ReadLine

--- main.c
+++ main.c
@@ -1,13 +1,46 @@
-void lsh_loop(void){
+#define RL_BUFSIZE 1024
+char *my_read_line(void){
+    int bufsize = RL_BUFSIZE;
+    int position = 0;
+    char *buffer = malloc(sizeof(char) * bufsize);
+    int c;
+
+    if(!buffer){
+        fprintf(stderr, "E: Allocation Error\n");
+        exit(EXIT_FAILURE);
+    }
+    while(1){
+        c = getchar();
+
+        if(c == EOF || c == '\n'){
+            buffer[position] = '\0';
+            return buffer;
+        }else{
+            buffer[position] = c;
+        }
+        position++;
+
+        if(position >= bufsize){
+            bufsize += RL_BUFSIZE;
+            buffer = realloc(buffer, bufsize);
+            if(!buffer){
+                frpintf(stderr, "E: Allocation Error\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
+}
+
+void my_loop(void){
     char *line;
     char **args;
     int status;
 
     do{
         printf(">> ");
-        line = lsh_read_line();
-        args = lsh_split_line(line);
-        status = lsh_execute(args);
+        line = my_read_line();
+        args = my_split_line(line);
+        status = my_execute(args);
 
         free(line);
         free(args);
@@ -19,7 +52,7 @@ int main(int argc, char **argv){
     
     //Run command Loop
     
-    lsh_loop();
+    my_loop();
 
     //Perform any shutdown or cleanup.
     
