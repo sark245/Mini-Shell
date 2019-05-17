f361721dd2680f702a1983ea1cc8e5a78be2d86f
sark245 <sark24599@gmail.com>
2019-05-18 00:47:48 +0530

    Added New function to tokenize the instruction passed

--- main.c
+++ main.c
@@ -1,36 +1,39 @@
 #define RL_BUFSIZE 1024
+#define TOK_BUFSIZE 64
+#define TOK_DELIM " \t\r\n\a"
 char *my_read_line(void){
-    int bufsize = RL_BUFSIZE;
-    int position = 0;
-    char *buffer = malloc(sizeof(char) * bufsize);
-    int c;
-
-    if(!buffer){
-        fprintf(stderr, "E: Allocation Error\n");
-        exit(EXIT_FAILURE);
+    char *line = NULL;
+    ssize_t bufsize = 0;
+    getline(&line, &bufsize, stdin);
+    return line;
+}
+char **split_line(char *line){
+    int bufsize = TOK_BUFSIZE, position = 0;
+    char **tokens = malloc(bufsize *sizeof(char*));
+    char *token;
+    if(!token){
+    fprintf(stderr,"E: allocation error\n");
+    exit(EXIT_FAILURE);
     }
-    while(1){
-        c = getchar();
-
-        if(c == EOF || c == '\n'){
-            buffer[position] = '\0';
-            return buffer;
-        }else{
-            buffer[position] = c;
-        }
+    
+    token = strtok(line,TOK_DELIM);
+    while(token != NULL){
+        tokens[position] = token;
         position++;
 
         if(position >= bufsize){
-            bufsize += RL_BUFSIZE;
-            buffer = realloc(buffer, bufsize);
-            if(!buffer){
-                frpintf(stderr, "E: Allocation Error\n");
+            bufsize += TOK_BUFSIZE;
+            tokens = realloc(tokens, bufsize *sizeof(char*));
+            if(!tokens){
+                fprintf(stderr, "E: Allocation Error\n");
                 exit(EXIT_FAILURE);
             }
         }
+        token = strtok(NULL, TOK_DELIM);
     }
+    tokens[position] = NULL;
+    return tokens;
 }
-
 void my_loop(void){
     char *line;
     char **args;
