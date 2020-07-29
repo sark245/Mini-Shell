42078191fcb96b8808dc3f7d6d4f943b835c2c80
Sarthak Kathuria <34064296+sark245@users.noreply.github.com>
2020-07-29 22:03:20 +0530

    Add files via upload

---
+++ Designs/OOPS/BlackJack/dealer.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "deck.h"
+#include "player.h"
+
+class Dealer{
+private:
+    vector<int> hand;
+    vector<Player*> playerSet;
+
+public: 
+    void setPlayerSet(vector<Player*> &playerSet);
+    vector<Player*> getPlayerSet();
+    void Distribute();
+    void Hit();
+    int CheckHand();
+};
