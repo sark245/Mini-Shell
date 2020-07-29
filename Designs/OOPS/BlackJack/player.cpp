6408ac11b4c13166ab7ded645af55fe622c0c9d9
Sarthak Kathuria <34064296+sark245@users.noreply.github.com>
2020-07-29 22:05:09 +0530

    Add files via upload

---
+++ Designs/OOPS/BlackJack/player.cpp
@@ -0,0 +1,31 @@
+#include "player.h"
+
+void Player::Hit(){
+    this->hand.push_back(getCard());
+}
+
+void Player::Stand(){
+
+}
+
+void Player :: Double(){}
+
+void Player :: Split(){}
+
+int Player :: CheckHand(){
+    int sum = 0;
+    for(auto i : this->hand){
+        if(i == 1 and sum + 11 <= 21) i = 11;
+        cout<<"Card: "<<i<<endl;
+        sum += i;
+    }
+    return sum;
+}
+
+char Player :: choice(){
+    cout<<"Non Blackjack, Hit or Stand?"<<endl;
+    cout<<"Press H to draw a card, press S to stand."<<endl;
+    char c = ' ';
+    cin>>c;
+    return c;
+}
