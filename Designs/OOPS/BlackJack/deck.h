454c6aead75722bfce0435477eb1a36b2d07f785
Sarthak Kathuria <34064296+sark245@users.noreply.github.com>
2020-07-29 22:03:45 +0530

    Add files via upload

---
+++ Designs/OOPS/BlackJack/deck.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include<iostream>
+#include<vector>
+#include<stdlib.h>
+#include<time.h>
+
+using namespace std;
+
+void init_deck();
+void shuffler();
+void print_deck();
+int getCard();
+bool is_deck_empty();
