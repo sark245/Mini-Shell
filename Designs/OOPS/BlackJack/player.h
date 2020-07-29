4c65869e3f3c8be4e7d88316caa9fbb9dedb90e7
Sarthak Kathuria <34064296+sark245@users.noreply.github.com>
2020-07-29 22:04:16 +0530

    Add files via upload

---
+++ Designs/OOPS/BlackJack/player.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "deck.h"
+
+class Player{
+private:
+    vector<int> hand;
+
+public: 
+    void Hit();
+    void Stand();
+    void Double();
+    void Split();
+    int CheckHand();
+    char choice();
+};
